ccae596243fbef93fe9034d1375e01c72c6cbe1f
willhua2008 <willhua2008@hotmail.com>
2023-12-22 12:08:57 +0800

    new js file

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-  res.send('测试中文是否好用！')
+  res.send('Hello World!' + '测试中文是否好用！');
+  res.send('测试中文是否好用2！');
 });
 
 app.listen(3000, () => {
